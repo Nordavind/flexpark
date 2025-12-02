a8001d27b1f53ac4c456ca0ac80dfdc05bf7eb41
Cristobal Mauras <cristobal.mauras@gmail.com>
2025-12-02 20:05:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // navbar
+  const navbar = document.getElementById("navbarMbDeploy");
+  const hambLink = document.getElementById('hambLink');
+  const navWrapper = navbar.querySelector('ul');
+
+  const toggleNavBar = (navbarElement, hambLink) => {
+    hambLink.classList.toggle("active");
+    navbarElement.classList.toggle("open");
+  };
+
+  hambLink.addEventListener('click', () => toggleNavBar(navbar, hambLink));
+
+  navWrapper.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      toggleNavBar(navbar, hambLink);
+    }
+  });
+});
